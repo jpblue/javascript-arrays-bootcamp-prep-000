46ac18f065b0ad8a7c482ffed665d14b9db3e9c7
jayhpaq@yahoo.com <jayhpaq@yahoo.com>
2018-05-30 20:58:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,5 @@ function addElementToEndOfArray (array, element) {
 function destructivelyAddElementToEndOfArray (array, element) { array.push(element)
 return (array)
 }
+
+function accessElementInArray (array, index)
